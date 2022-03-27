b665a7c5d5825413832eb87b43bbb2ce7cf11909
nurik <nuriknurbolot313@gmail.com>
2022-03-27 14:59:03 +0300

    Дз

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// задание 1
+        String firsName = "Ivan ";
+        String middleName ="Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName  + firsName  + middleName;
+        System.out.println("ФИО сотрудника - "+fullName);
+        // задание 2
+         String upperFullName = fullName.toUpperCase(Locale.ROOT);
+        System.out.println(upperFullName);
+        // задание 3
+        String fulName = "Иванов Семён Семёнович";
+                fulName = fulName.replace("ё", "е");
+        System.out.println(fulName);
+
     }
 }
